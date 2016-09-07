30a5ec84aaecadf64f66c79f11e20bdce8751276
Ethan Adams <ethanadamsm@gmail.com>
2016-09-07 14:37:32 -0600

    Fix healthbar scale for bar

--- healthbar.py
+++ healthbar.py
@@ -4,8 +4,8 @@ class Healthbar():
 	def __init__(self, x, y, health):
 		self.x = x
 		self.y = y
-		self.w = 75
-		self.h = 25
+		self.w = 75.0
+		self.h = 25.0
 		self.totalhealth = health
 		self.health = health
 		self.image = pygame.image.load("healthbar.png")
@@ -13,8 +13,12 @@ class Healthbar():
 	def render(self, screen):
 		if self.health < 0:
 			self.health = 0
-		self.image = pygame.transform.scale(self.image, (75 * (self.health / self.totalhealth), 25))
+		self.image = pygame.transform.scale(self.image, (int(75.0 * (self.health / self.totalhealth)), 25))
 		screen.blit(self.image, (self.x, self.y))
+
+	def update(self):
+		if self.health < 0:
+			self.health = 0
 
 	def setX(self, x):
 		self.x = x
