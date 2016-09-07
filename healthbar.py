a7658d28be529d68898a6b47b712e400d9e399c9
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-07 13:09:27 -0600

    added transformations for health

--- healthbar.py
+++ healthbar.py
@@ -4,10 +4,14 @@ class Healthbar():
 	def __init__(self, x, y, health):
 		self.x = x
 		self.y = y
+		self.w = 75
+		self.h = 25
+		self.totalhealth = health
 		self.health = health
 		self.image = pygame.image.load("healthbar.png")
 
 	def render(self, screen):
+		self.image = pygame.transform.scale(self.image, (75 * (self.health / self.totalhealth), 25))
 		screen.blit(self.image, (self.x, self.y))
 
 	def setX(self, x):
@@ -15,3 +19,9 @@ class Healthbar():
 
 	def setY(self, y):
 		self.y = y
+
+	def setHealth(self, health):
+		self.health = health
+
+	def getHealth(self):
+		return self.health
