5cebae06c3b2ae223d138f962e82e35fb839f31b
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-12 12:54:50 -0600

    add get item and render inventory box

--- inventory.py
+++ inventory.py
@@ -3,6 +3,8 @@ import sys, pygame
 class Inventory(object):
 	def __init__(self):
 		self.itemList = []
+		self.image = pygame.image.load("inventory.png")
+		self.font = pygame.font.SysFont("comicsansms", 30)
 
 	def addItem(self, item):
 		self.itemList.append(item)
@@ -10,6 +12,20 @@ class Inventory(object):
 	def render(self, screen):
 		for item in self.itemList:
 			item.render(screen)
+		screen.blit(self.image, (10, 420))
+		x = 20
+		y = 430
+		num = 1
+		for item in self.itemList:
+			scalex = 50 / item.getW()
+			scaley = 50 / item.getH()
+			screen.blit
+			cur = pygame.transform.scale(item.getImage(), (scalex, scaley))
+			screen.blit(item.getImage(), (x, y))
+			text = self.font.render(str(num), True, (0, 0, 0))
+			screen.blit(text, (x, 380))
+			x += 50
+			num += 1
 
 	def update(self):
 		for item in self.itemList:
@@ -37,3 +53,6 @@ class Inventory(object):
 
 	def getH(self):
 		return self.itemList[0].getH()
+
+	def getItem(self, index):
+		return self.itemList[index]
