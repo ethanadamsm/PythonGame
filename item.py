6221ed49435e23441ad0840cd0d80f2212841aca
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-16 12:55:00 -0600

    add update for item x

--- item.py
+++ item.py
@@ -4,6 +4,10 @@ class Item():
 	def __init__(self, x, y, image):
 		self.x = x
 		self.y = y
+		self.vx = 0
+		self.vy = 0
+		self.a = False
+		self.d = False
 		self.image = image
 
 	def render(self, screen):
@@ -20,3 +24,33 @@ class Item():
 
 	def getImage(self):
 		return self.image
+
+	def getX(self):
+		return self.x
+
+	def getY(self):
+		return self.y
+
+	def setX(self, x):
+		self.x = x
+
+	def setY(self, y):
+		self.y = y
+
+	def getVelX(self):
+		return self.vx
+
+	def getVelY(self):
+		return self.vy
+
+	def setVelX(self, vx):
+		self.vx = vx
+
+	def setVelY(self, vy):
+		self.vy = vy
+
+	def update(self, d, a):
+		if d:
+			self.x -= 1
+		elif a:
+			self.x += 1
