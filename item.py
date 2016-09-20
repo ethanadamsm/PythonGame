5400d5b18847ac53908af8c57d6b7e03c843eab8
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-20 12:55:40 -0600

    add more getters/setters

--- item.py
+++ item.py
@@ -1,17 +1,21 @@
 import sys, pygame
 
 class Item():
-	def __init__(self, x, y, image):
+	def __init__(self, x, y, w, h, image):
 		self.x = x
 		self.y = y
+		self.w = w
+		self.h = h
 		self.vx = 0
 		self.vy = 0
 		self.a = False
 		self.d = False
+		self.visible = True
 		self.image = image
 
 	def render(self, screen):
-		screen.blit(self.image, (int(self.x), int(self.y)))
+		if self.visible:
+			screen.blit(self.image, (int(self.x), int(self.y)))
 
 	def getImage(self):
 		return self.image
@@ -24,12 +28,6 @@ class Item():
 
 	def getImage(self):
 		return self.image
-
-	def getX(self):
-		return self.x
-
-	def getY(self):
-		return self.y
 
 	def setX(self, x):
 		self.x = x
@@ -54,3 +52,15 @@ class Item():
 			self.x -= 1
 		elif a:
 			self.x += 1
+
+	def getW(self):
+		return self.w
+
+	def getH(self):
+		return self.h
+
+	def setVisible(self, visible):
+		self.visible = visible
+
+	def getVisible(self):
+		return self.visible
