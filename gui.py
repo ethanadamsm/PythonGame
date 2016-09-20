843124b7d9f4fbc3a2a5c1a2a831fd4511eb9afc
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-20 12:55:25 -0600

    add box methods

--- gui.py
+++ gui.py
@@ -16,12 +16,25 @@ class Gui:
 			self.image = pygame.transform.scale(self.image, (self.w, self.h))
 			for box in self.boxes:
 				box.render(screen)
+				print(box)
 
 	def setVisible(self, visible):
 		self.visible = visible
+
+	def setBoxVisible(self, index, visible):
+		self.boxes[index].setVisible(visible)
 
 	def addBox(self, x, y, w, h):
 		self.boxes.append(box.Box(x, y, w, h))
 
 	def addImage(self, index, image):
 		self.boxes[index].addImage(image)
+
+	def setBoxText(self, index, text):
+		self.boxes[index].setText(text)
+
+	def setBoxFontSize(self, index, fontSize):
+		self.boxes[index].setFontSize(fontSize)
+
+	def getBoxText(self, index):
+		return self.boxes[index].getText()
