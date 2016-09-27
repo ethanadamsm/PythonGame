aba1e17a058e51c22dea108aa8a90cb5a5360692
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-27 12:41:44 -0600

    button class start and create

---
+++ button.py
@@ -0,0 +1,44 @@
+import sys, pygame
+
+class Button:
+	def __init__(self, x, y, w, h):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.visible = False
+		self.font = pygame.font.SysFont("comicsansms", 30)
+		self.image = ""
+		self.text = 0
+
+	def render(self, screen):
+		if self.visible and self.image != "":
+			screen.blit(self.image, (self.x, self.y))
+		if self.text != 0:
+			text = self.font.render(self.text, True, (0, 0, 0))
+			screen.blit(text, (self.w / 2) + self.x, (self.h / 2) + self.y)
+
+	def addImage(self, image):
+		self.image = image
+		self.image = pygame.transform.scale(self.image, (self.w, self.h))
+		self.visible = True
+
+	def setFontSize(self, size):
+		self.font = pygame.font.SysFont("comicsansms", size)
+
+	def setText(self, text):
+		self.text = text
+
+	def getText(self):
+		return self.text
+
+	def setVisible(self, visible):
+		self.visible = visible
+
+	def getCollision(self):
+		return checkCollision
+
+	def checkCollision():
+		x = pygame.mouse.get_pos()[0]
+		y = pygame.mouse.get_pos()[1]
+		return (x > self.x and x < self.x + self.w and y > self.y and y < self.y + self.h)
