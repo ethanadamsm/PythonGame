c894f6d51ffb3490211b2c3f26fa6b6bb78ba36b
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-09-30 13:16:22 -0600

    staff class start and create

---
+++ staff.py
@@ -0,0 +1,63 @@
+import sys, pygame
+
+class Staff:
+	def __init__(self, x, y, w, h, image):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.image = image
+		self.vx = 0
+		self.vy = 0
+		self.visible = True
+		self.typee = "Staff"
+
+	def render(self, screen):
+		if self.visible:
+			screen.blit(self.image, (self.x, self.y))
+
+	def update(self):
+		self.x += self.vx
+		self.y += self.vy
+
+	def getX(self):
+		return self.x
+
+	def getY(self):
+		return self.y
+
+	def getW(self):
+		return self.w
+
+	def getH(self):
+		return self.h
+
+	def getVelX(self):
+		return self.vx
+
+	def getVelY(self):
+		return self.vy
+
+	def setVelX(self, vx):
+		self.vx = vx
+
+	def setVelY(self, vy):
+		self.vy = vy
+
+	def setX(self, x):
+		self.x = x
+
+	def setY(self, y):
+		self.y = y
+
+	def getImage(self):
+		return self.image
+
+	def getImages(self):
+		return self.image
+
+	def setVisible(self, visible):
+		self.visible = visible
+
+	def getType(self):
+		return self.typee
