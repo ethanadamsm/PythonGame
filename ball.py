eb2277f99e341cfdd30cc7b39e9b0c734607f12a
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-10-03 12:48:26 -0600

    ball class start and create

---
+++ ball.py
@@ -0,0 +1,30 @@
+import sys, pygame
+
+class Ball():
+	def __init__(self, x, y, w, h, image):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.vx = 0
+		self.vy = 0
+		self.image = image
+
+	def render(self, screen):
+		screen.blit(self.image, (self.x, self.y))
+
+	def update(self):
+		self.x += self.vx
+		self.y += self.vy
+
+	def getVelX(self):
+		return self.vx
+
+	def getVelY(self):
+		return self.vy
+
+	def setVelX(self, vx):
+		self.vx = vx
+
+	def setVelY(self, vy):
+		self.vy = vy
